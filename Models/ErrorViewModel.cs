a6378481aa63d3b2433ad5a6c0a616a9fea65072
JustinChenMedia <justinchenmedia99@gmail.com>
2026-05-14 00:40:09 +1000

    Initialised an ASP.NET Core Web App (MVC)
    Using Visual Studio 2022

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FavouriteBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
